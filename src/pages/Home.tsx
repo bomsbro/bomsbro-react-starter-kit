068fcc5fe31afd7e7f40cf186d6a2f781004ca1b
bomsbro <qorwogjs@gmail.com>
2025-12-09 13:43:34 +0900

    fix: remove contents

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,8 @@
-import { Link } from 'react-router-dom';
-
 const Home = () => {
   return (
     <div>
       <h2>🏠 홈페이지</h2>
       <p>React Router와 상태관리를 학습하는 프로젝트입니다.</p>
-      <ul>
-        <li><Link to="/counter">카운터</Link> - useState 기본 사용법</li>
-        <li><Link to="/timer">타이머</Link> - useEffect 기본 사용법</li>
-        <li><Link to="/todos">투두리스트</Link> - Context API 상태관리</li>
-      </ul>
     </div>
   );
 };
