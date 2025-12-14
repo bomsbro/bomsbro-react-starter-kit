f3e67bbc444e9ca515a98deb98be053bd1f7fb2e
bomsbro <qorwogjs@gmail.com>
2025-12-15 00:52:21 +0900

    fix: type

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -5,7 +5,7 @@ import App from '@app/App'
 
 import '@app/globals.css'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
   </StrictMode>
